81527bc71ad1cbb6aa472561ef9e17b65ee8be1a
bhatanagha <49675714+bhatanagha@users.noreply.github.com>
2021-12-18 18:56:29 +0530

    feat: section 9 done

--- section-8/section-8/src/components/user.js
+++ section-8/section-8/src/components/user.js
@@ -1,31 +1,39 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './user.css'
 const User = (props) => {
 
-    const [isValid, setValidName] = useState(false)
-    const [name, setEnteredName] = useState('')
-    const [age, setEnteredAge] = useState('')
+    const nameInpRef = useRef() // using ref
+    const ageInpRef = useRef()
 
-    const nameChangeHandler = (e) => {
-        setEnteredName(e.target.value)
-    }
+    const [isValid, setValidName] = useState(true) // using state
+    // const [name, setEnteredName] = useState('')
+    // const [age, setEnteredAge] = useState('')
 
-    const ageChangeHandler = (e) => {
-        setEnteredAge(e.target.value)
-    }
+    // const nameChangeHandler = (e) => {
+    //     setEnteredName(e.target.value)
+    // }
+
+    // const ageChangeHandler = (e) => {
+    //     setEnteredAge(e.target.value)
+    // }
 
     function submitHandler(e) {
-        if (name.trim().length > 0) {
+        const enteredName = nameInpRef.current.value
+        const enteredAge = ageInpRef.current.value
+
+        if (enteredName.trim().length > 0) {
             e.preventDefault()
             setValidName(true)
             const data = {
-                name: name,
-                age: age
+                name: enteredName, // or name
+                age: enteredAge    // or age  (from state)
             }
             props.newUserData(data)
-            setEnteredName('')
-            setEnteredAge('')
-        }
+            nameInpRef.current.value = ''
+            ageInpRef.current.value = ''
+            // setEnteredName('')
+            // setEnteredAge('')
+        } else setValidName(false)
     }
     
   return (
@@ -34,12 +42,20 @@ const User = (props) => {
     <div className="User">
        <div className="new-expense__control">
             <label>Name</label> 
-            <input className={`name-input ${isValid ? '' : 'invalid'}`} type="text" value={name} onChange={nameChangeHandler}></input>
+            <input 
+            className={`name-input ${!isValid ? 'invalid' : ''}`} type="text"
+            //  value={name}
+            //   onChange={nameChangeHandler}
+            ref={nameInpRef}
+            ></input>
         </div>
 
         <div className="new-expense__control">
             <label>Age</label>
-            <input type="number" value={age} onChange={ageChangeHandler}></input>
+            <input type="number"
+            //  value={age}
+            //  onChange={ageChangeHandler}
+             ref={ageInpRef}></input>
         </div>
     <button type="button" onClick={submitHandler}>Add User</button>
     <button type="button">Cancel</button>
